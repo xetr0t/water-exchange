090583b2d4bc54ecb72ed2e01c85250a7e56a083
xetr0t <faffellek@gmail.com>
2021-03-22 12:51:20 +0000

    Initial commit

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0xF40b49b69A217f79839c0960b2b4e00CC756E945',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xa70bd5d28ea7a4c10393c8110d9fcc2e18d421d0'
+}
+
+export default priceContracts
